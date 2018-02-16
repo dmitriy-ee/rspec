e7847b72091a846267f8d73d5b5d3dd84d9c5e33
dmitriy_ee <dmitriy_ee@hotmail.com>
2018-02-16 20:49:52 +0200

    add car.rb

---
+++ car.rb
@@ -0,0 +1,27 @@
+class Car
+
+  MILES_PER_GALON = 20
+
+  attr_reader :fuel
+
+  def initialize
+    @fuel = 0
+  end
+
+  def add_fuel amount
+    @fuel += amount
+  end
+
+#How long we drive...
+
+  def range
+    0
+  end
+
+end
+
+car = Car.new
+
+car.add_fuel 10
+
+puts "Range is #{car.range}" #We can drive ... MILES
